ea6a88e85a0bf082644a1f0b2e612980bc5398b9
AhmedSharaf <a.moataz88@gmail.com>
2022-09-16 10:35:08 +0200

    Finishing the script

---
+++ script.py
@@ -0,0 +1,35 @@
+#Inputs to put into the paragraph
+adj_1 = input("Enter an adjective: ")
+noun_1 = input("Enter a plural noun: ")
+noun_2 = input("Enter a noun: ")
+adv_1 = input("Enter an adverb: ")
+num_1 = input("Enter a number: ")
+verb_1 = input("Enter a past tense verb: ")
+adj_2 = input("Enter an -est adjective: ")
+verb_2 = input("Enter a past tense verb: ")
+adv_2 = input("Enter an adverb: ")
+adj_3 = input("Enter and adjective: ")
+
+#Madlib
+madlib = f"Today, my fabulous camp group went to a (an)\
+{adj_1} amusement park. It was a\
+fun park with lots of cool {noun_1}\
+and enjoyable play structures. When we got there, my\
+kind counselor shouted loudly, \"Everybody off the\
+{noun_2}.\" We all pushed out in a terrible\
+hurry. My counselor handed out yellow tickets, and\
+we scurried in. I was so excited! I couldn't figure out\
+what exciting thing to do first. I saw a scary roller\
+coaster I really liked so, I {adv_1} ran\
+over to get in the long line that had about\
+{num_1} people in it. When I finally\
+got on the roller coaster I was {verb_1}. In fact I was so nervous my two knees\
+were knocking together. This was the {adj_2} ride I had ever been on! In about two\
+minutes I heard the crank and grinding of the gears.\
+That’s when the ride began! When I got to the bottom,\
+I was a little {verb_2} but I\
+was proud of myself. The rest of the day went\
+{adv_2}. It was a(n) {adj_3} day at the fun park."
+
+#Print the madlib
+print(madlib)
